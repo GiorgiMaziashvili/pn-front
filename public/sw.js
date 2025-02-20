7eabdfee93960f8718801ad24c8324349cbf85b6
Giorgi Maziashvili <giorgim@redberry.ge>
2025-02-21 01:46:15 +0400

    Enhance push notification handling and add notification click functionality

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,47 @@
-self.addEventListener("push", (event) => {
-    const data = event.data ? event.data.json() : {};
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: "/icon.png",
-    });
-  });
+// self.addEventListener("push", (event) => {
+//     const data = event.data ? event.data.json() : {};
+//     console.log(data);
+//     self.registration.showNotification(data.title, {
+//       body: data.body,
+//       icon: "/icon.png",
+//       badge: "/icon.png",
+//       vibrate: [200, 100, 200, 100, 200, 100, 200],
+//       data: {
+//         url: JSON.parse(event.message).url
+//       }
+//     });
+//   });
+self.addEventListener('push', (event) => {
+  const data = event.data.json();
+  const title = data.title;
+  const options = {
+    body: data.body,
+    icon: "/icon.png",
+    badge: "/icon.png",
+    vibrate: [200, 100, 200, 100, 200, 100, 200],
+    data: {
+      url: data?.url
+    }
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+
+  const url = event.notification.data.url;
+
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then((clientList) => {
+      const client = clientList.find((client) => client.url === url && 'focus' in client);
+      if (client) {
+        client.focus();
+      } else {
+        clients.openWindow(url);
+      }
+    })
+  );
+});
