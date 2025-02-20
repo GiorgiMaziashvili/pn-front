9f0af639563a7ef3d92c52eb5e595f7f80aeb5c2
Giorgi Maziashvili <giorgim@redberry.ge>
2025-02-21 00:41:18 +0400

    seconed

---
+++ app/welcome/notification.ts
@@ -0,0 +1,21 @@
+export const requestNotificationPermission = async () => {
+    const permission = await Notification.requestPermission();
+    if (permission !== "granted") {
+      console.warn("Permission denied");
+      return;
+    }
+  
+    const registration = await navigator.serviceWorker.ready;
+    const subscription = await registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: "BOLbW36jelHonTWQ-FEk4s8SydCSauck1y1HIQ6GhrWHEgrONIdDrI-KapqyltyePVlrGGDggSwaqutJBRol-lw", 
+    });
+  
+    console.log("Push Subscription:", JSON.stringify(subscription));
+  
+    await fetch("https://pn-back.onrender.com/subscribe", {
+      method: "POST",
+      body: JSON.stringify(subscription),
+      headers: { "Content-Type": "application/json" },
+    });
+};
